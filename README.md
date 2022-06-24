3b9c81e11721d993fdb33e9aa7a8d449727296fd
Yulia <yulia@Andys-MacBook-Pro.local>
2022-06-24 13:18:57 +0300

    get_next_line

---
+++ README.md
@@ -0,0 +1,6 @@
+# Get_next_line
+## Description
+May it be a file, stdin, or even later a network connection, you will always need a way to read content line by line. It is time to start working on this function, which will be essential for your future projects.
+## Compilation
+`gcc -Wall -Wextra -Werror -D BUFFER_SIZE=32 get_next_line.c get_next_line_utils.c
+`
---
+++ main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdarrin <fdarrin@student.42.fr>          +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/06 16:29:19 by fdarrin          #+#    #+#              */
+/*   Updated: 2020/08/06 23:17:33 by fdarrin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+
+int
+	main(void)
+{
+	int		r;
+	char	*line;
+
+	line = NULL;
+	while ((r = get_next_line(0, &line)) > 0)
+	{
+		printf("%s\n", line);
+		free(line);
+		line = NULL;
+	}
+	printf("%s", line);
+	free(line);
+	line = NULL;
+}
